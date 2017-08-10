bd0eb0f37fb6e16485e6b5dd5b4968988861a14e
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-08-11 00:11:08 +0200

    encryption working

--- routes/handlers/handlePostLogin.js
+++ routes/handlers/handlePostLogin.js
@@ -1,16 +1,16 @@
 const fs = require('fs')
 const strftime = require('strftime')
+const crypto = require('crypto')
 
 function handlePostLogin (req, res) {
   const { email, password } = req.body
   let autentification = false
-  // let usersArr
 
   fs.readFile('./data-db/users_txt.txt', 'utf-8', (err, data) => {
     if (err) throw err
-    const usersArr = data.split('\r\n') // (/\r?\n/)
-    // console.log(usersArr)
-    usersArr.forEach((user) => {
+    const usersArrEncrypted = data.split('\r\n') // (/\r?\n/)
+    const usersArrDecrypted = usersArrEncrypted.map((aAuthLine) => decrypt(aAuthLine))
+    usersArrDecrypted.forEach((user) => {
       let [emailDB, passDB] = user.split(':')
       if (emailDB === email && passDB === password) {
         autentification = true
@@ -25,6 +25,13 @@ function handlePostLogin (req, res) {
 
     autentification ? res.redirect('/home') : res.redirect('/error')
   })
+}
+
+function decrypt (text) {
+  let decipher = crypto.createDecipher(global.algorithm, global.cryptoPass)
+  let dec = decipher.update(text, 'hex', 'utf8')
+  dec += decipher.final('utf8')
+  return dec
 }
 
 module.exports = handlePostLogin
--- routes/handlers/handlePostRegistry.js
+++ routes/handlers/handlePostRegistry.js
@@ -1,19 +1,18 @@
 const fs = require('fs')
 const strftime = require('strftime')
 const crypto = require('crypto')
-const algorithm = 'aes-256-ctr'
-const createId = () => '_' + Math.random().toString(36).substr(2, 9)
-const cryptoPass = createId()
 
 function handlePostRegistry (req, res) {
   const { email, password } = req.body
-  const dataToWrite = '\r\n' + email + ':' + password
+  const dataToWrite = email + ':' + password
+  const encryptedData = '\r\n' + encrypt(dataToWrite)
   let existentUser = false
 
   fs.readFile('./data-db/users_txt.txt', 'utf-8', function (err, data) {
     if (err) throw err
-    const usersArr = data.split('\r\n')
-    usersArr.forEach((user) => {
+    const usersArrEncrypted = data.split('\r\n')
+    const usersArrDecrypted = usersArrEncrypted.map((aAuthLine) => decrypt(aAuthLine))
+    usersArrDecrypted.forEach((user) => {
       let [emailDB] = user.split(':')
       if (emailDB === email) {
         existentUser = true
@@ -23,13 +22,14 @@ function handlePostRegistry (req, res) {
       console.log('Email already in use')
       res.redirect('/regist/')
     } else {
-      fs.appendFile('./data-db/users_txt.txt', dataToWrite, function (err) {
+      fs.appendFile('./data-db/users_txt.txt', encryptedData, function (err) {
         if (err) throw err
-        console.log('------ New registry ------')
+        console.log('------ New registry --------------')
         console.log('email: ' + email)
         console.log('password: ' + password)
         console.log('registered at: ' + strftime('%F:%T', new Date()))
-        console.log('--------------------------')
+        console.log('encrypted as: ' + encryptedData)
+        console.log('----------------------------------')
         res.redirect('/')
       })
     }
@@ -37,14 +37,14 @@ function handlePostRegistry (req, res) {
 }
 
 function encrypt (text) {
-  let cipher = crypto.createCipher(algorithm, cryptoPass)
+  let cipher = crypto.createCipher(global.algorithm, global.cryptoPass)
   let crypted = cipher.update(text, 'utf8', 'hex')
   crypted += cipher.final('hex')
   return crypted
 }
 
 function decrypt (text) {
-  let decipher = crypto.createDecipher(algorithm, cryptoPass)
+  let decipher = crypto.createDecipher(global.algorithm, global.cryptoPass)
   let dec = decipher.update(text, 'hex', 'utf8')
   dec += decipher.final('utf8')
   return dec
